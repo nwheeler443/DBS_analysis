a3dc8b30b72d42548ae6f7c596230130491c4bf4
nwheeler443 <nwheeler443@gmail.com>
2015-02-25 13:15:31 +1300

    adding p-values to final output file

--- plotresults.R
+++ plotresults.R
@@ -2,7 +2,7 @@ plotresults <- function(comm, path, xaxvalues=c(-10,10), breaks=500) {
     dbs <- read.table(paste(comm, "-", path, "/results.dbs", sep=""))
     info <- read.delim(paste("genomes/", path, ".info", sep=""), header=F)
     
-	genelist <- genelist <- cbind(dbs[,c(1,2,3,10)], info[match(dbs[,2], info[,1]), 2:ncol(info)])
+	genelist <- genelist <- cbind(dbs[,c(1,2,3,10,12)], info[match(dbs[,2], info[,1]), 2:ncol(info)])
     write.table(genelist, file=paste(comm, "-", path, "/genelist.txt", sep=""), quote=F, sep="\t", col.names=F, row.names=F)
     
     png(paste(comm, "-", path, "/graph.png", sep=""), width=1000, height=1000, res=150)
