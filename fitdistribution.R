fedf0cfca2276580e142053922482c91dd3c1931
nwheeler443 <nwheeler443@gmail.com>
2015-02-25 14:56:50 +1300

    changed fitdist cutoff to p=0.01

--- fitdistribution.R
+++ fitdistribution.R
@@ -3,7 +3,7 @@ fitdistribution <- function(comm, patho) {
 	dbs <- read.table(paste(comm, "-", patho, "/results.dbs",sep=""), header=F)
 	n <- nrow(dbs[dbs$V10!=0,])
 
-	dist <- rnorm(n=n, mean=0, sd=sd(dbs[dbs$V12>0.05,10]))
+	dist <- rnorm(n=n, mean=0, sd=sd(dbs[dbs$V12>0.01,10]))
 	distdens <- density(dist, bw=0.3)
 
 	hist(dbs[dbs$V10!=0,10], breaks=500, xlim=c(-7,7), freq=FALSE, ylim=c(0,0.5))
