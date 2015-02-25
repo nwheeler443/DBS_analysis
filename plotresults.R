75e105f18cad77ed399c19e98d752ead2caf4730
nwheeler443 <nwheeler443@gmail.com>
2015-02-25 13:18:13 +1300

    removing synonymous changes from distribution graph

--- plotresults.R
+++ plotresults.R
@@ -6,7 +6,7 @@ plotresults <- function(comm, path, xaxvalues=c(-10,10), breaks=500) {
     write.table(genelist, file=paste(comm, "-", path, "/genelist.txt", sep=""), quote=F, sep="\t", col.names=F, row.names=F)
     
     png(paste(comm, "-", path, "/graph.png", sep=""), width=1000, height=1000, res=150)
-    hist(dbs[,10], breaks=breaks, xlim=xaxvalues, main=paste("Results for ", comm, " vs ", path, sep=""), xlab="Delta bitscore")
+    hist(dbs[dbs$V10!=0,10], breaks=breaks, xlim=xaxvalues, main=paste("Results for ", comm, " vs ", path, sep=""), xlab="Delta bitscore")
     library(moments)
     text((xaxvalues[2]/2), nrow(dbs[-0.5<dbs$V10 & dbs$V10<0.5,])/2, paste("skewness = ", round(skewness(dbs[,10]), 2)))
     dev.off()
