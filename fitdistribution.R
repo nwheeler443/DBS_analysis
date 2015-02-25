18cb3c5bf429167e851fbae74199f91ebaf34c68
nwheeler443 <nwheeler443@gmail.com>
2015-02-25 14:49:33 +1300

    turned fitdistribution into a function

--- fitdistribution.R
+++ fitdistribution.R
@@ -1,15 +1,12 @@
-comm="enteritidis"
-patho="gallinarum"
-
-#fitdistribution <- function(comm, patho) {
+fitdistribution <- function(comm, patho) {
 	
-dbs <- read.table(paste(comm, "-", patho, "/results.dbs",sep=""), header=F)
-n <- nrow(dbs[dbs$V10!=0,])
+	dbs <- read.table(paste(comm, "-", patho, "/results.dbs",sep=""), header=F)
+	n <- nrow(dbs[dbs$V10!=0,])
 
-dist <- rnorm(n=n, mean=0, sd=sd(dbs[dbs$V12>0.05,10]))
-distdens <- density(dist, bw=0.3)
+	dist <- rnorm(n=n, mean=0, sd=sd(dbs[dbs$V12>0.05,10]))
+	distdens <- density(dist, bw=0.3)
 
-hist(dbs[dbs$V10!=0,10], breaks=500, xlim=c(-7,7), freq=FALSE, ylim=c(0,0.5))
-lines(distdens$x, distdens$y)
+	hist(dbs[dbs$V10!=0,10], breaks=500, xlim=c(-7,7), freq=FALSE, ylim=c(0,0.5))
+	lines(distdens$x, distdens$y)
 
-#}
+}
